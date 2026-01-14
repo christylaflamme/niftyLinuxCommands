72b5df06ac4d36d56aefd8d96da86609379e401d
christylaflamme <65321420+christylaflamme@users.noreply.github.com>
2026-01-14 10:56:55 -0600

    Delete files.sh

--- files.sh
+++
@@ -1,7 +0,0 @@
-# how to zip a list of files together
-# zip = command
-# files.zip = names of zipped files document
-# -@ = argument to take a file list
-# zip.lst = list of files
-zip files.zip -@ < zip.lst
-
